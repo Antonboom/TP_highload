5f280ab08dc0579535ae3786db10406fcb3eeb77
a.telishev <anton.telishev@yandex.ru>
2016-09-27 20:35:31 +0300

    thread_pool

--- httpd.rb
+++ httpd.rb
@@ -3,10 +3,10 @@
  ## 
 
 require 'socket'
+require 'concurrent'
 
 require './config'
 require './handlers'
-require './threadpool'
 require './optparse'
 
 options = get_options()
@@ -30,19 +30,41 @@ if options.has_key?(:ncpu)
 	ncpu = options[:ncpu]
 end
 
+
+work_queue = Queue.new
 server = TCPServer.new host, port
 puts "Listen #{host} on #{port}..."
-pool = ThreadPool.new ncpu
 
 
-# Kernel.trap('INT') do
-#   server.close
-#   puts " - The server shuts down..."
-#   exit
-# end
+pool = Concurrent::ThreadPoolExecutor.new(
+  :min_threads => [2, ncpu].max,
+  :max_threads => [2, ncpu].max,
+  :max_queue   => [2, ncpu].max * 100,
+  :fallback_policy => :caller_runs
+)
+
+
+future = Concurrent::Future.execute(:executor => pool) do
+  while true
+		if !work_queue.empty?
+			client = work_queue.pop(true) rescue nil
+			if client
+				main_handler(client)
+			end
+	  end
+	end
+end
 
 
 loop do 
-  pool.put_client(server.accept)  
-  pool.run
+  work_queue << server.accept  
+end
+
+
+Kernel.trap('INT') do
+  server.close
+  puts " - The server shuts down..."
+  pool.shutdown
+  main_thread.exit
+  exit
 end
